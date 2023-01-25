b831e64205f622da826e9da24def7db83b239488
rohmatsetiadi <rohmatsetiadi4@gmail.com>
2023-01-25 21:28:07 +0700

    push

---
+++ script.js
@@ -0,0 +1,8 @@
+function ubahMode(){
+    const ubah = document.body;
+    ubah.classList.toggle("dark");
+
+    [].forEach.call(ubah.getElementsByTagName("div"), function (el) {
+        el.classList.toggle('dark')
+    });
+}
